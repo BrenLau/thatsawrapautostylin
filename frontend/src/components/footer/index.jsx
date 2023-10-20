fd393db7f635245773312128d9661a735a50c91d
JehanIbrahem <jehan.ibrahem25@gmail.com>
2023-10-20 12:00:44 -0700

    got the footer started

---
+++ frontend/src/components/footer/index.jsx
@@ -0,0 +1,22 @@
+import "./footer.css"
+import { SocialIcon } from 'react-social-icons'
+import { NavLink } from 'react-router-dom'
+
+const Footer = () => {
+
+    const email = () => {
+        window.open('mailto:thatsawrapautostylin@gmail.com/?subject=&body=')
+    }
+
+    return (
+        <footer className="footer">
+            <span>Vinson Zheng</span>
+            <SocialIcon className="email" network='email' onClick={email}></SocialIcon>
+            <SocialIcon url="https://www.instagram.com/thatsawrapautostylin/?igshid=MzRlODBiNWFlZA%3D%3D" />
+            {/* <i className="fa-brands fa-instagram"></i> */}
+        </footer>
+    )
+
+}
+
+export default Footer
