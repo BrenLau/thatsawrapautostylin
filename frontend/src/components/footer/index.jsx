5ec358cfe1f2e3af5aab5f4398c8b50b240edaa5
JehanIbrahem <jehan.ibrahem25@gmail.com>
2023-10-20 13:01:26 -0700

    added logo to footer

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -1,4 +1,5 @@
 import "./footer.css"
+import img from '../../assets/TAW-logo.jpg';
 
 const Footer = () => {
 
@@ -8,6 +9,8 @@ const Footer = () => {
 
     return (
         <footer className="footer">
+            <img className='footer-logo' src={img} />
+            
             <span>Vinson Zheng</span>
             <span onClick={email} className="email"><i className="fa-regular fa-envelope"></i></span>
             <a href='https://www.instagram.com/thatsawrapautostylin/?igshid=MzRlODBiNWFlZA%3D%3D'><i className="fa-brands fa-instagram"></i></a>
