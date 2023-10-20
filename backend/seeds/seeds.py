9f7f01f8785438d278e81694be2cfe9a044cdac9
Michael <Michael_oyola@outlook.com>
2023-10-20 17:35:59 -0400

    added seeder data

---
+++ backend/seeds/seeds.py
@@ -0,0 +1,31 @@
+from ..models import db,User, CarType, SCHEMA
+from sqlalchemy.sql import text
+
+
+
+def seed_data():
+    admin = User(
+        email='thatsawrapautostylin@gmail.com', name='Admin', password='Adminpassword', is_admin=True,
+    )
+
+
+    Sedan = CarType(
+        car_type='Coupe/Sedan'
+    )
+
+    Suv = CarType(
+        car_type='SUV/Truck'
+    )
+
+    db.session.add_all(admin, Sedan, Suv)
+
+
+    def undo_seeds():
+        if environment == "production":
+            db.session.execute(f"TRUNCATE table {SCHEMA}.car_types RESTART IDENTITY CASCADE;")
+            db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        else:
+            db.session.execute(text("DELETE FROM car_types"))
+            db.session.execute(text("DELETE FROM users"))
+
+        db.session.commit()
