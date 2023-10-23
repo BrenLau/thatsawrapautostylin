da4f8be2cc0ab864723ab3760fa80702da431a1e
Philip McCrickard <pcricket10@gmail.com>
2023-10-23 16:52:56 -0400

    basic layout of aboutme/contact done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Footer from './components/footer'
 import './App.css'
 import Nav from './components/nav'
+import AboutMe from './components/about-me'
 
 function App() {
 
   return (
     <>
       <Nav />
+      <AboutMe />
       <Footer/>
     </>
   )
--- frontend/src/components/about-me/index.jsx
+++ frontend/src/components/about-me/index.jsx
@@ -1,8 +1,24 @@
 import './about-me.css';
+import ContactForm from './contact-form';
 
 const AboutMe = () => {
   return (
-    <h1>test</h1>
+    <div id="about-me">
+      <h3 id='about-me-title'>about me/contact</h3>
+      <div id="map-contact-container">
+        <div id='google-map'>
+          Google Map Here
+        </div>
+        <div id="contact-info">
+          <p>address: 123 Sesame Street</p>
+          <p>phone: (555) 867-5309</p>
+          <p>email: john-doe@example.com</p>
+          <p>socials: FB,whatever</p>
+
+        </div>
+      </div>
+      <ContactForm />
+    </div>
   )
 }
 
