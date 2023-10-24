2aa880528b02206c6d11e8d991df7a37e149f74c
JonTabiendo <jontabiendo@gmail.com>
2023-10-24 14:16:36 -0400

    about-me booking merged

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,17 +1,18 @@
 import MenuButton from './menu-button';
 import img from '../../assets/TAW-logo.jpg';
+// import { useHistory } from 'react-router-dom'
 import './nav.css';
 
 const Nav = () => {
 
-  
+
   return (
     <div id="nav">
       <div id='nav-logo-div'>
-        <img id='nav-logo' src={img} />
+        <img id='nav-logo' src={img}/>
       </div>
       <div id='nav-title-div'>
-          <h2 id='nav-title'>thatsawrapautostylin'</h2>
+          <h2 id='nav-title'>thatsawrapautostylin&apos;</h2>
       </div>
       <div id="nav-menu-div">
         <MenuButton />
