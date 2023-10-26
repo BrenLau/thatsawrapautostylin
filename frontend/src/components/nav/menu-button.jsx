c6091fa6b39b217a1ba0ba9f3f5a0f09440fc5e1
JonTabiendo <jontabiendo@gmail.com>
2023-10-26 14:00:32 -0400

    signup modal started

--- frontend/src/components/nav/menu-button.jsx
+++ frontend/src/components/nav/menu-button.jsx
@@ -2,11 +2,17 @@ import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import OpenModalButton from "../open-modal-button";
 import LoginFormModal from "../login-modal";
+import SignupFormModal from "../signup-form-modal";
 
-const MenuButton = ({ user }) => {
+const MenuButton = () => {
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
   const [transitioning, setTransitioning] = useState(false)
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("user")) || null)
+
+  const updateUserState = (user) => {
+    setUser(user)
+  }
 
   useEffect(() => {
     if (!showMenu) return;
@@ -31,6 +37,19 @@ const MenuButton = ({ user }) => {
     }
   };
 
+  const logout = async () => {
+    const response = await fetch("/api/auth/logout", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      sessionStorage.removeItem("user")
+      setUser(null)
+    }
+  }
+
   const dropdownClassname = "menu-dropdown-div" + (showMenu ? "" : "-hidden")
 
   return (
@@ -39,14 +58,14 @@ const MenuButton = ({ user }) => {
       {!user ? (
         <div id={dropdownClassname}>
           {transitioning || !showMenu ? null : <Link to='/booking'className="menu-dropdown-button">Book Now</Link>}
-          {transitioning || !showMenu ? null : <OpenModalButton modalComponent={<LoginFormModal />} buttonText={"Login"}/>}
-          {transitioning || !showMenu ? null : <OpenModalButton modalComponent={<LoginFormModal />} buttonText={"Sign Up"}/> }
+          {transitioning || !showMenu ? null : <OpenModalButton modalComponent={<LoginFormModal updateUser={updateUserState} />} buttonText={"Login"}/>}
+          {transitioning || !showMenu ? null : <OpenModalButton modalComponent={<SignupFormModal />} buttonText={"Sign Up"}/> }
         </div>
       ) : (
         <div id={dropdownClassname}>
           {transitioning || !showMenu ? null : <p className="menu-dropdown-button">{user.email}</p>}
-          {transitioning || !showMenu ? null : <p className="menu-dropdown-button">Book Now</p>}
-          {transitioning || !showMenu ? null : <p className="menu-dropdown-button">Log Out</p>}
+          {transitioning || !showMenu ? null : <Link to='/booking'className="menu-dropdown-button">Book Now</Link>}
+          {transitioning || !showMenu ? null : <p className="menu-dropdown-button" onClick={logout}>Log Out</p>}
         </div>
       )}
     </div>
