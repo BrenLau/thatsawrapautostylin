ba2d2ab5f1c227e617a07b07fd043cb5eacd46af
JehanIbrahem <jehan.ibrahem25@gmail.com>
2023-10-27 11:20:26 -0700

    working on booking form backend

--- backend/forms/booking_form.py
+++ backend/forms/booking_form.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, DateTimeField
 from wtforms.validators import DataRequired, Email, ValidationError
 from backend.models import Booking
 
 class BookingForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    
+    email= StringField('email', validators=[DataRequired()])
+    number = IntegerField('number', validators=[DataRequired()])
+    instagram = StringField('instagram', validators=[DataRequired()])
+    car = StringField('car', validators=[DataRequired()])
+    times = DateTimeField('times', validators=[DataRequired()])
