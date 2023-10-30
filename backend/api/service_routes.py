3697e47f86b496579b5c6dc5db3e31ae2d8a70f2
BrenLau <blau4000@gmail.com>
2023-10-30 16:35:45 -0400

    post services

--- backend/api/service_routes.py
+++ backend/api/service_routes.py
@@ -3,7 +3,7 @@ from backend.models import User, db, Service
 from backend.forms import LoginForm
 # from backend.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
-
+from ..forms.service_form import CreateServiceForm
 service_routes = Blueprint('services', __name__)
 
 
@@ -12,3 +12,22 @@ def getServices():
     services = Service.query.all()
 
     return {"services": [service.to_dict() for service in services]}
+
+
+# @login_required
+@service_routes.route('/', methods=['POST'])
+def addServices():
+    form = CreateServiceForm()
+    service = Service(
+        title=form.data['title'],
+        description=form.data['description'],
+        image_url=form.data['image_url'],
+        car_type=form.data['car_type'],
+        duration=form.data['duration'],
+        price=form.data['price']
+    )
+
+    db.session.add(service)
+    db.session.commit()
+
+    return service.to_dict()
--- frontend/src/components/services/AddService.jsx
+++ frontend/src/components/services/AddService.jsx
@@ -21,8 +21,30 @@ const AddService = () => {
         }
         getCarTypes()
     }, [])
+
+    const onSubmit = async (e) => {
+        const data = {
+            car_type: carTypeSelected,
+            title,
+            description,
+            price,
+            duration: time,
+            image_url: 'fdsa'
+        }
+        e.preventDefault()
+        const response = await fetch('http://127.0.0.1:5000/api/services', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })
+        if (response.ok) {
+            const res = await response.json()
+            console.log(res)
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <label>
                 Service Title
                 <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
@@ -52,8 +74,8 @@ const AddService = () => {
                     return (
                         <label key={car.id}>
                             {car.car_type}
-                            < input type="radio" value={car.car_type} checked={carTypeSelected === car.car_type ? true : false} onClick={(e) => {
-                                setCarTypeSelected(car.car_type)
+                            < input type="radio" value={car.id} checked={carTypeSelected === car.id ? true : false} onClick={(e) => {
+                                setCarTypeSelected(car.id)
                             }}></input>
 
                         </label>
@@ -66,6 +88,7 @@ const AddService = () => {
                 Duration(Hours)
                 <input type="number" value={time} onChange={(e) => setTime(e.target.value)}></input>
             </label>
+            <button>Submit</button>
 
         </form >
     )
