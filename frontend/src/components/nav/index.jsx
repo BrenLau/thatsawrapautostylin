8add2085e7682a67209161a76f4d3b4cdf615a02
JehanIbrahem <jehan.ibrahem25@gmail.com>
2023-10-30 13:09:58 -0700

    new branch

---
+++ frontend/src/components/nav/index.jsx
@@ -0,0 +1,21 @@
+
+
+const Nav = () => {
+
+  
+  return (
+    <div id="nav">
+      <div id='nav-logo'>
+
+      </div>
+      <div id='nav-title'>
+
+      </div>
+      <div id="nav-menu">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
