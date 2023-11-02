4a37e208cf51e20961dcad75eea399b1866ff966
JonTabiendo <jontabiendo@gmail.com>
2023-11-02 00:09:22 -0400

    react calendar api working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,17 +7,35 @@ import './index.css'
 
 export const UserContext = createContext();
 
+import ApiCalendar from "react-google-calendar-api";
+
+const config = {
+	clientId: "762633836570-vpbro17viheb27tl43n2v7qq76aljd8b.apps.googleusercontent.com",
+  apiKey: "AIzaSyC1UIZ4AhrqAxk_7mc3R2RUjlwoJvZaKbI",
+  scope: "https://www.googleapis.com/auth/calendar",
+  discoveryDocs: [
+		"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ],
+};
+
+const apiCalendar = new ApiCalendar(config);
+export const CalendarContext = createContext();
+
 function Root() {
 	const [user, setUser] = useState(null);
 	const userState = { user, setUser }
+	console.log(apiCalendar)
+
 
 	return (
 		<ModalProvider>
 			<UserContext.Provider value={userState}>
-				<BrowserRouter>
-					<App />
-					<Modal />
-				</BrowserRouter>
+				<CalendarContext.Provider value={apiCalendar}>
+					<BrowserRouter>
+						<App />
+						<Modal />
+					</BrowserRouter>
+				</CalendarContext.Provider>
 			</UserContext.Provider>
 		</ModalProvider>
 	);
